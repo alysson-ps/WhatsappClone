72c3df0c9348b20b191fced497992b38ec12b543
Alisson santos <craftgames.alysson@gmail.com>
2020-12-18 14:05:56 -0300

    feat(app): app style

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import Call from './pages/calls'
 import Message from './pages/messages'
 import Status from './pages/status'
 
-import { Container } from './App.css';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    justify-content: center;
+    align-items: center;
+    flex:1;
+`
 
 export default function App() {
 	return (
